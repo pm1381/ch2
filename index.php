6d47aceb66d71d9c8a83c70c704b083f51733aba
parham <Parham_minoo@yahoo.com>
2023-02-28 23:46:47 -0800

    adding php docker

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 date_default_timezone_set("Asia/Tehran");
-define("BASE", __DIR__);
-require BASE . '/vendor/autoload.php';
-require BASE . '/src/configs/setup.php';
+require __DIR__ . '/vendor/autoload.php';
+
+dd("hello world");
+
+require __DIR__ . '/src/configs/setup.php';
